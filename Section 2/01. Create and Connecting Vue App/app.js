22369d984d3e8fdac6d77319b0b8b70bbb9217fe
David Yappeter <davidyap11les@gmail.com>
2022-02-08 09:32:52 +0700

    section2: working with data inside app (methods)

--- Section 2/01. Create and Connecting Vue App/app.js
+++ Section 2/01. Create and Connecting Vue App/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
   data: function () {
     return {
-      courseGoal: "Finish the course and Learn Vue!",
+      courseGoalA: "Finish the course and Learn Vue!",
+      courselGoalB: "Master Vue and build Amazing apps!",
       vueLink: "https://vuejs.org/",
     };
   },
@@ -9,9 +10,9 @@ const app = Vue.createApp({
     outputGoal() {
       const randomNumber = Math.random();
       if (randomNumber < 0.5) {
-        return "Learn Vue!";
+        return this.courseGoalA;
       } else {
-        return "Master Vue!";
+        return this.courselGoalB;
       }
     },
   },
