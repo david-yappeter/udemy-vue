212b02f6e514dc209025ad34e91da0e92303376a
David Yappeter <davidyap11les@gmail.com>
2022-02-09 09:15:44 +0700

    Section 3: More about v-for and 'key' on loop

--- Section 3/01. Conditional Rendering/app.js
+++ Section 3/01. Conditional Rendering/app.js
@@ -11,6 +11,9 @@ const app = Vue.createApp({
       this.goals.push(this.enteredGoal);
       this.enteredGoal = "";
     },
+    removeGoal(e, idx) {
+      this.goals.splice(idx, 1);
+    },
   },
 });
 
